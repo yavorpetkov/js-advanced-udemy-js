78ec25aeb86bbb3fc33454d6583b49bc8998bce8
Yavor Petkov <hermsues@gmail.com>
2021-06-18 13:44:42 +0300

    intro to DOM

---
+++ 04.DOM/02.manipulators/js/01.innerText&TextContent.js
@@ -0,0 +1,13 @@
+const h1 = document.querySelector('h1');
+const ul = document.querySelector('ul');
+const p = document.querySelector('#main');
+
+// innerText
+// sellects the text in the h1 tag
+h1.innerText = "I'm Hungry!";
+// sellects the text in the ul tags and merges it into one string
+ul.innerText;
+
+// textContent
+// shows everything like it is in the html and even display none items
+p.textContent;
