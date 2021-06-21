7813ec8771589180cbc70d2f1c2c8638473669af
Yavor Petkov <hermsues@gmail.com>
2021-06-21 15:12:30 +0300

    refactoring the NBA charts exercise

--- 04.DOM/02.manipulators/js/13.NBAScoresChart.js
+++ 04.DOM/02.manipulators/js/13.NBAScoresChart.js
@@ -85,14 +85,58 @@ const warriorsGames = [
 		}
 	}
 ];
-const ulParent = document.createElement('ul');
-for (const game of warriorsGames) {
-	const { homeTeam, awayTeam } = game;
-	const gameLi = document.createElement('li');
+
+// *************************
+// Part 1
+// *************************
+
+// const ulParent = document.createElement('ul');
+// for (const game of warriorsGames) {
+// 	const { homeTeam, awayTeam } = game;
+// 	const gameLi = document.createElement('li');
+// 	// destructuring awayTeam and homeTeam
+// 	const { team: hTeam, points: hPoins } = homeTeam;
+// 	const { team: aTeam, points: aPoints } = awayTeam;
+
+// 	const teams = `${aTeam} @ ${hTeam}`;
+// 	// bolding the winning score
+// 	let scoreLine;
+// 	if (aPoints > hPoins) {
+// 		scoreLine = `<b>${aPoints}</b>-${hPoins}`;
+// 	} else {
+// 		scoreLine = `${aPoints}-<b>${hPoins}</b>`;
+// 	}
+// 	const warriors = hTeam === 'Golden State' ? homeTeam : awayTeam;
+// 	gameLi.classList.add(warriors.isWinner ? 'win' : 'loss');
+
+// 	gameLi.innerHTML = `${teams} ${scoreLine}`;
+// 	ulParent.append(gameLi);
+// }
+// document.body.prepend(ulParent);
+
+// *************************
+// Part 2 REFACTOR
+// *************************
+const makeChart = (games, targetTeam) => {
+	const ulParent = document.createElement('ul');
+	for (const game of games) {
+		const { homeTeam, awayTeam } = game;
+		const gameLi = document.createElement('li');
+		// getting the scores
+		gameLi.innerHTML = getScoreLine(game);
+		// get the homeTeam / did it win / the add it as a list element
+		gameLi.classList.add(isWinner(game, targetTeam) ? 'win' : 'loss');
+
+		ulParent.append(gameLi);
+	}
+	return ulParent;
+};
+
+const getScoreLine = ({ homeTeam, awayTeam }) => {
 	// destructuring awayTeam and homeTeam
 	const { team: hTeam, points: hPoins } = homeTeam;
 	const { team: aTeam, points: aPoints } = awayTeam;
-
+	// create team name string
 	const teams = `${aTeam} @ ${hTeam}`;
 	// bolding the winning score
 	let scoreLine;
@@ -101,10 +145,19 @@ for (const game of warriorsGames) {
 	} else {
 		scoreLine = `${aPoints}-<b>${hPoins}</b>`;
 	}
-	const warriors = hTeam === 'Golden State' ? homeTeam : awayTeam;
-	gameLi.classList.add(warriors.isWinner ? 'win' : 'loss');
+	return `${teams} ${scoreLine}`;
+};
 
-	gameLi.innerHTML = `${teams} ${scoreLine}`;
-	ulParent.append(gameLi);
-}
-document.body.prepend(ulParent);
+const isWinner = ({ homeTeam, awayTeam }, targetTeam) => {
+	const target = homeTeam.team === targetTeam ? homeTeam : awayTeam;
+	return target.isWinner;
+};
+
+const gsSection = document.querySelector('#gs');
+const houstonSection = document.querySelector('#hr');
+
+const chart1 = makeChart(warriorsGames, 'Golden State');
+const chart2 = makeChart(warriorsGames, 'Houston');
+
+gsSection.appendChild(chart1);
+houstonSection.appendChild(chart2);
