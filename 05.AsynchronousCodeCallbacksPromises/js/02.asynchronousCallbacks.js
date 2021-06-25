1041298fdea84a8709e518f652dc29141d991114
Yavor Petkov <hermsues@gmail.com>
2021-06-25 12:46:53 +0300

    asynvhronous callbacks demo

---
+++ 05.AsynchronousCodeCallbacksPromises/js/02.asynchronousCallbacks.js
@@ -0,0 +1,11 @@
+console.log('I HAPPEN FIRST!');
+// alert('Hi there!'); //This stops everything
+console.log('I HAPPEN SECOND!');
+
+console.log('I HAPPEN FIRST!');
+setTimeout(function() {
+	//This does not hold everything up
+	//JS relies on the browser to keep track of the timer
+	console.log('I HAPPEN THIRD!');
+}, 1000);
+console.log('I HAPPEN SECOND!');
