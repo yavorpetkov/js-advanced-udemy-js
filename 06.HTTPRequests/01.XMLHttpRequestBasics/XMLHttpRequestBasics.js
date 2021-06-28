754084716e174cbb9dd0f11299c0703793787603
Yavor Petkov <hermsues@gmail.com>
2021-06-28 09:51:42 +0300

    XML Http requests basics

--- 06.HTTPRequests/01.XMLHttpRequestBasics/XMLHttpRequestBasics.js
+++ 06.HTTPRequests/01.XMLHttpRequestBasics/XMLHttpRequestBasics.js
@@ -1,6 +1,7 @@
 const firstReq = new XMLHttpRequest();
 firstReq.addEventListener('load', function() {
 	console.log('IT WORKED!!!');
+	// THIS does not work with arrow functions
 	const data = JSON.parse(this.responseText);
 	for (let planet of data.results) {
 		console.log(planet.name);
@@ -9,6 +10,8 @@ firstReq.addEventListener('load', function() {
 firstReq.addEventListener('error', () => {
 	console.log('ERROR!!!!!!');
 });
+// this takes time and done from the browser
 firstReq.open('GET', 'https://swapi.dev/api/planets/');
 firstReq.send();
+// so this will be run first
 console.log('Request Sent!');
